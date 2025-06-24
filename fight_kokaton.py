a91d9c37a1ae667e27e66d54b18a52e8e18d4edd
YakumoKazama <c0a2406442@edu.teu.ac.jp>
2025-06-24 17:47:52 +0900

    不要なデバッグを削除

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -217,8 +217,6 @@ def main():
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
                 beams.append(Beam(bird))
-                for expl in explosions:
-                    print(expl.life, end=", ")
         screen.blit(bg_img, [0, 0])
         
         for i, bomb in enumerate(bombs):
